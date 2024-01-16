9f36745e31453773f996cd1cd01f5cecc44440e4
Narasimha swamy s r <133078935+NarasimhaSwamy202@users.noreply.github.com>
2024-01-16 23:51:53 +0530

    Update 01_ Flipping the Matrix.py

--- Python (Basic) Certification Test/01_ Flipping the Matrix.py
+++ Python (Basic) Certification Test/01_ Flipping the Matrix.py
@@ -9,17 +9,15 @@
 # column reverse operations 
 
 def maxSum(mat): 
-  
+    R = len(mat)         #Rows
+    C = len(mat[0])      #Columns
     Sum = 0
     for i in range(0, R // 2): 
         for j in range(0, C // 2): 
   
             r1, r2 = i, R - i - 1
             c1, c2 = j, C - j - 1
-  
-            # We can replace current cell [i, j] 
-            # with 4 cells without changing/affecting 
-            # other elements. 
+
             Sum += max(max(mat[r1][c1], mat[r1][c2]), 
                        max(mat[r2][c1], mat[r2][c2])) 
   
